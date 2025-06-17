7e3779d096018c99751502b33de07b19e9ab45ec
aurelie31000 <103678739+aurelie31000@users.noreply.github.com>
2025-06-17 15:55:27 +0200

    Initial Hello World with basic test and package structure

---
+++ src/main/python/hello_world.py
@@ -0,0 +1,9 @@
+# src/main/python/hello_world.py
+def get_message():
+    return "Hello World"
+
+def main():
+    print(get_message())
+
+if __name__ == "__main__":
+    main()
---
+++ src/test/python/test_hello_world.py
@@ -0,0 +1,14 @@
+# src/test/python/test_hello_world.py
+import unittest
+# Utilise l'import absolu grâce aux fichiers __init__.py
+from src.main.python.hello_world import get_message
+
+class TestHelloWorld(unittest.TestCase):
+    def test_hello_world_message(self):
+        self.assertEqual(get_message(), "Hello World")
+
+    def test_true_equals_true(self):
+        self.assertTrue(True == True)
+
+if __name__ == '__main__':
+    unittest.main()
